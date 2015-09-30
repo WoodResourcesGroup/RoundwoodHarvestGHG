708bd563c411b1f6e5afc72f008cf17b76d4a68e
Peter Tittmann <ptittmann@gmail.com>
2015-09-30 14:42:00 -0700

    updated tenyear_harv to carbon

--- fcat_views.sql
+++ fcat_views.sql
@@ -1,3 +1,12 @@
+create or replace function ccf2c(double precision, 
+       	  	  	   	spgrav double precision default 0.45, 
+				mc double precision default 0.12) returns double precision
+       as 'select $1 * spgrav * 62.4 * 0.5 * 0.00045359237'
+       language sql
+       immutable
+       returns null on null input;
+
+
 --note that this assumes 2012 end use proportions from McIver et al.
 create or replace view hrv_by_enduse as select
        	     year,
@@ -18,16 +27,16 @@ create or replace view tenyear_harv as
 with foo as (select min(year)+10 mny from umt_harvest)
      select s1.year-10 year_start,
      	    s1.year year_end, 
-     	    avg(s2.private)::numeric(8,2) as priv10y_avg,
-	    stddev(s2.private)::numeric(8,2) as priv10y_stddev, 
-     	    avg(s2.state)::numeric(8,2) as state10y_avg,
-	    stddev(s2.state)::numeric(8,2) as state10y_stddev, 
-     	    avg(s2.tribal)::numeric(8,2) as tribal10y_avg,
-	    stddev(s2.tribal)::numeric(8,2) as tribal10y_stddev,
-     	    avg(s2.blm)::numeric(8,2) as blm10y_avg,
-	    stddev(s2.blm)::numeric(8,2) as blm10y_stddev,
-     	    avg(s2.nationalforests)::numeric(8,2) as nf10y_avg,
-	    stddev(s2.nationalforests)::numeric(8,2) as nf10y_stddev  
+     	    ccf2c(avg(s2.private))::numeric(8,2) as priv10y_avg,
+	    ccf2c(stddev(s2.private))::numeric(8,2) as priv10y_stddev, 
+     	    ccf2c(avg(s2.state))::numeric(8,2) as state10y_avg,
+	    ccf2c(stddev(s2.state))::numeric(8,2) as state10y_stddev, 
+     	    ccf2c(avg(s2.tribal))::numeric(8,2) as tribal10y_avg,
+	    ccf2c(stddev(s2.tribal))::numeric(8,2) as tribal10y_stddev,
+     	    ccf2c(avg(s2.blm))::numeric(8,2) as blm10y_avg,
+	    ccf2c(stddev(s2.blm))::numeric(8,2) as blm10y_stddev,
+     	    ccf2c(avg(s2.nationalforests))::numeric(8,2) as nf10y_avg,
+	    ccf2c(stddev(s2.nationalforests))::numeric(8,2) as nf10y_stddev  
 	    from umt_harvest s1, 
 	    	 umt_harvest s2,
 		 foo
