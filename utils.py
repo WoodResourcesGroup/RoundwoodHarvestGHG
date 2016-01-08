277dc7abcecd27b5e7177e89f56529d757474974
Peter Tittmann <ptittmann@gmail.com>
2016-01-08 15:45:48 -0800

    harvested wood products calculations

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine as ce
 import pandas as pd
 import sqlite3
 import os
-
+import numpy as np
 
 gUrl = 'https://docs.google.com/spreadsheets/d/{0}/export?gid={1}&format=csv'
 
@@ -32,3 +32,21 @@ def csv2sqlite(dbcon, csvfile):
     df = pd.read_csv(csvfile)
     fn = os.path.basename(os.path.splitext(csvfile)[0])
     df.to_sql(fn, dbcon, if_exists='replace')
+
+
+def distFromRange(total, maxr=32, minr=2):
+    av = (maxr + minr)/2
+    stdev = (float(maxr) - float(minr))/4
+    d_frac = (total - np.floor(total)) * \
+        np.random.normal(av, stdev, 1).clip(min=0)[0]
+    t_bdt = np.random.normal(av, stdev, np.floor(total)).clip(min=0)
+    return np.append(t_bdt, d_frac)
+
+
+def sumFromDist(total, maxr=0.32, minr=0.02):
+    av = (maxr + minr)/2
+    stdev = (float(maxr) - float(minr))/4
+    d_frac = (total - np.floor(total)) * \
+        np.random.normal(av, stdev, 1).clip(min=0)[0]
+    t_bdt = np.sum(np.random.normal(av, stdev, np.floor(total)).clip(min=0))
+    return (d_frac+t_bdt)
