200583a59b1e3bf31d67d0203890c3047373129a
Peter Tittmann <ptittmann@gmail.com>
2017-01-04 09:22:00 -0800

    Major updates to BC GWP calculations
    Completely rehashed BC GWP calculations using an new function in
    utils.py. Merged two redundant sections of the document.
    On branch master
    Your branch is up-to-date with 'origin/master'.
    
    Changes to be committed:
    	modified:   biomass_burning.ipynb
    	modified:   calfire.org
    	modified:   calfire.pdf
    	modified:   calfire.tex
    	modified:   fcat_biomass.sqlite -- added a new table from Ward
    	1989 for use in BC GWP calcs
    	modified:   utils.py
    	modified:   wood_fates.ipynb

--- utils.py
+++ utils.py
@@ -89,5 +89,68 @@ def pm2bcgwpPiles(pm, gwp=3200):
     ecS = ecSest*tcS
     ecF = ecFest*tcF
     ec = ecS + ecF
-    return ec * gwp
 
+
+def ward2db():
+    """
+    pulls Table 2 from Ward 1989 transposed for sql easieness
+    """
+    ward = gData('13UQtRfNBSJ81PXxbYSnB2LrjHePNcvhJhrsxRBjHpoY', 1488869919)
+    ward.to_sql('ward89_2',sqlitedb('fcat_biomass')['cx'],if_exists = 'replace')
+    
+def pm2bcPiles(pm, est='estimate', gwp=3200):
+    db = sqlitedb('fcat_biomass')
+    # Step 1: Detetermine the S:F ratio for piled fuels
+    # From Ward 1989 table V (Regional Composite for Piled Slash)
+    fRatio = 0.9
+    sRatio = 1-fRatio
+
+    # Step 2: Calculate PM from S and F
+    pmF = pm * fRatio
+    pmS = pm * sRatio
+
+    # Step 3: Determine PM:BC ratio for flaming and smoldering.
+    # Based on Ward 1989 Table 2 this requires knowing the PM:TC (Total Carbon) ratio.
+    fTCQuery = "select avg(tc)/100 tc, avg(tc_coefv) from ward89_2 where combustion = 'f' and profile in ('tractor','crane');"
+    fTC,fTCcv=db['crs'].execute(fTCQuery).fetchall()[0]
+    sTCQuery = "select avg(tc)/100 tc, avg(tc_coefv) from ward89_2 where combustion = 's' and profile in ('tractor','crane');"
+    sTC,sTCcv=db['crs'].execute(sTCQuery).fetchall()[0]
+
+    #Then the TC:BC ratios for smoldering and flaming
+    fBCQuery = "select avg(tc)/100 tc, avg(tc_coefv) from ward89_2 where combustion = 'f' and profile in ('tractor','crane');"
+    fBC,fBCcv=db['crs'].execute(fBCQuery).fetchall()[0]
+    sBCQuery = "select avg(tc)/100 tc, avg(tc_coefv) from ward89_2 where combustion = 's' and profile in ('tractor','crane');"
+    sBC,sBCcv=db['crs'].execute(sBCQuery).fetchall()[0]
+
+    #Estimate for Black Carbon from flaming phase
+    tcFest = pmF * fTC
+    bcFest = tcFest * fBC
+    #High stimate for Black Carbon from flaming phase
+    tcFhigh = tcFest + (fTCcv * tcFest)
+    bcFhigh = tcFhigh * fBC + ((tcFhigh * fBC)*fBCcv)
+
+    #Low stimate for Black Carbon from flaming phase
+    tcFlow = tcFest - (fTCcv * tcFest)
+    bcFlow = tcFlow * fBC - ((tcFlow * fBC)*fBCcv)
+    
+    #Estimate for Black Carbon from smoldering phase
+    tcSest = pmS * sTC
+    bcSest = tcSest * sBC
+
+    #High stimate for Black Carbon from smoldering phase
+    tcShigh = tcSest + (sTCcv * tcSest)
+    bcShigh = tcShigh * sBC + ((tcShigh * sBC)*sBCcv)
+    #Estimate for Black Carbon from smoldering phase
+    bcSest = pmS * sTC * sBC
+
+    #Low stimate for Black Carbon from smoldering phase
+    tcSlow = tcSest - (sTCcv * tcSest)
+    bcSlow = tcSlow * sBC - ((tcSlow * sBC)*sBCcv)
+
+
+    results = {'estimate': (bcFest + bcSest) * gwp,
+               'high': (bcFhigh + bcShigh) *gwp,
+               'low': (bcFlow +bcSlow) * gwp
+               }
+
+    return results[est]
