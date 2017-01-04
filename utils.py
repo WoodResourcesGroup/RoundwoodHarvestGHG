4e35f30a676046b86567fc523f180f334913d4cf
Peter Tittmann <ptittmann@gmail.com>
2017-01-04 14:50:39 -0800

    Finalized Emissions calcs
    TODO: calculate uncombusted piles CO2e
    TODO: polish final sections
    
    On branch master
    Your branch is up-to-date with 'origin/master'.
    
    Changes to be committed:
    	modified:   calfire.org
    	modified:   calfire.pdf
    	modified:   calfire.tex
    	modified:   fcat.bib
    	modified:   fcat_biomass.sqlite
    	modified:   utils.py

--- utils.py
+++ utils.py
@@ -3,13 +3,14 @@ import pandas as pd
 import sqlite3
 import os
 import numpy as np
-from pint import UnitRegistry as UR
 
 gUrl = 'https://docs.google.com/spreadsheets/d/{0}/export?gid={1}&format=csv'
 
 dbname = 'fcat'
+sqltdb = 'fcat_biomass'
 engine = ce('postgresql:///{0}'.format(dbname), echo=True)
-
+ch4GWP = 28  # GWP of Methane
+mmbf2mcf = 5.44  # From McIver and Morgan
 
 def gData(key, gid, hrow=0, indx=None):
     if not indx:
@@ -20,7 +21,7 @@ def gData(key, gid, hrow=0, indx=None):
     return data
 
 
-def sqlitedb(dbname):
+def sqlitedb(dbname= sqltdb):
     fName = dbname+'.sqlite'
     conn = sqlite3.connect(fName)
     curs = conn.cursor()
@@ -61,42 +62,50 @@ def co2eDecomp(biomass):
     cMethane = 0.09
     cCO2 = 0.61
     cT = cFraction*biomass
-    ch4co2e = cT * cMethane * 56
+    ch4co2e = cT * cMethane * ch4GWP
     return ch4co2e + (cCO2 * cT)
 
+# Deprecated!!
+# def pm2bcgwpPiles(pm, gwp=3200):
+#     '''converts a PM 2.5 estimate produced
+#     from pile burns to an estimate of black carbon.
+#     returns tons BC and CO2e
+#     '''
+#     pctFlaming = 0.9
+#     pctSmolder = 0.5
+#     tcFest = 0.082
+# #   tcFcv = 0.09
+#     tcSest = 0.560
+# #   tcScv = 0.01
+#     ecFest = 0.082
+# #   ecFcv = 0.45
+#     ecSest = 0.029
+# #   ecScv = 0.49
 
-def pm2bcgwpPiles(pm, gwp=3200):
-    '''converts a PM 2.5 estimate produced
-    from pile burns to an estimate of black carbon.
-    returns tons BC and CO2e
-    '''
-    pctFlaming = 0.9
-    pctSmolder = 0.5
-    tcFest = 0.082
-#   tcFcv = 0.09
-    tcSest = 0.560
-#   tcScv = 0.01
-    ecFest = 0.082
-#   ecFcv = 0.45
-    ecSest = 0.029
-#   ecScv = 0.49
-
-    pmSm = pm*pctSmolder
-    pmF = pm*pctFlaming
-    tcS = tcSest * pmSm
-    tcF = tcFest * pmF
-    tc = tcS + tcF
-    ecS = ecSest*tcS
-    ecF = ecFest*tcF
-    ec = ecS + ecF
+#     pmSm = pm*pctSmolder
+#     pmF = pm*pctFlaming
+#     tcS = tcSest * pmSm
+#     tcF = tcFest * pmF
+#     tc = tcS + tcF
+#     ecS = ecSest*tcS
+#     ecF = ecFest*tcF
+#     ec = ecS + ecF
 
 
-def ward2db():
+def ward2db(dbname = 'fcat_biomass'):
     """
-    pulls Table 2 from Ward 1989 transposed for sql easieness
+    pulls tables from google spreadsheet to the sqlite database
     """
+    # Table 2 from Ward 1989 transposed for sql easieness
     ward = gData('13UQtRfNBSJ81PXxbYSnB2LrjHePNcvhJhrsxRBjHpoY', 1488869919)
-    ward.to_sql('ward89_2',sqlitedb('fcat_biomass')['cx'],if_exists = 'replace')
+    ward.to_sql('ward89_2',
+                sqlitedb(dbname)['cx'],
+                if_exists='replace')
+    bioEnergy = gData('138FWlGeW57MKdcz2UkWxtWV4o50SZO8sduB1R6JOFp8',
+                      529610043)
+    bioEnergy.set_index('producttype').transpose().to_sql('mciver_bio',
+                                                          sqlitedb(dbname)['cx'],
+                                                          if_exists = 'replace')
     
 def pm2bcPiles(pm, est='estimate', gwp=3200):
     db = sqlitedb('fcat_biomass')
@@ -154,3 +163,33 @@ def pm2bcPiles(pm, est='estimate', gwp=3200):
                }
 
     return results[est]
+
+
+def me(yr,vol):
+    """
+    calculates mill residues for a give volume of roundwood harvested (mmbf) amd year
+    """
+    me70 = 0.42 #Mill efficiency in 1970
+    me11 = 0.48 #Mill efficiency in 2011
+    ann = (me11 - me70)/(2011-1970)
+    if yr <= 1970:
+        return vol * (me70-((1970-yr)*ann))
+    if yr >= 1970:
+        return vol * (me70+((yr-1970)*ann))
+
+def lr(yr,vol):
+    """
+    probabilistic estimate of the volume of logging residuals for a given year and roundwood harvest in MMBF
+    """
+    lr04 = np.random.normal(0.0302,0.0123)
+    # 72% reduction in logging residue production between 1990 and 2011)
+    ann = 0.72/(2011-1990)
+    lr90 = lr04 + (lr04*0.72)
+    lr11 = lr04 - (lr04*((2011-2004)*ann))
+    # if year is before 1990, assume lr from 1990
+    if yr < 1990:
+        return vol * lr90
+    if yr >= 1990 and yr <=2004 :
+        return vol * (lr04 + (lr04*((2004-yr)*ann)))
+    if yr > 2004:
+        return vol * (lr04 - (lr04*((yr-2004)*ann)))
