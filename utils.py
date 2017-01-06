de15725875bf40f991dc879e794fb40aa9276c47
Peter Tittmann <ptittmann@gmail.com>
2017-01-05 23:57:56 -0800

    Resolved CARB and BOE pile estimates
    Need to restructure a little to make the case that:
    
    1. all CARB forestry emissions are not attributable to commercial harvest.
    2. CARB is likely over-estimaing forestry emissions
    3. present potential emissions from BOE piles as opposed to CARB in summary
    
    On branch master
    Your branch is up-to-date with 'origin/master'.
    
    Changes to be committed:
    	modified:   calfire.org
    	modified:   calfire.pdf
    	modified:   calfire.tex
    	modified:   fcat_biomass.sqlite
    	deleted:    milled.py
    	modified:   utils.py

--- utils.py
+++ utils.py
@@ -8,9 +8,16 @@ gUrl = 'https://docs.google.com/spreadsheets/d/{0}/export?gid={1}&format=csv'
 
 dbname = 'fcat'
 sqltdb = 'fcat_biomass'
-engine = ce('postgresql:///{0}'.format(dbname), echo=True)
-ch4GWP = 28  # GWP of Methane
-mmbf2mcf = 5.44  # From McIver and Morgan
+
+
+def sqlitedb(dbname=sqltdb):
+    fName = dbname+'.sqlite'
+    conn = sqlite3.connect(fName)
+    curs = conn.cursor()
+    return {'fname': fName,
+            'cx': conn,
+            'crs': curs}
+
 
 def gData(key, gid, hrow=0, indx=None):
     if not indx:
@@ -21,13 +28,50 @@ def gData(key, gid, hrow=0, indx=None):
     return data
 
 
-def sqlitedb(dbname= sqltdb):
-    fName = dbname+'.sqlite'
-    conn = sqlite3.connect(fName)
-    curs = conn.cursor()
-    return {'fname': fName,
-            'cx': conn,
-            'crs': curs}
+engine = ce('postgresql:///{0}'.format(dbname), echo=True)
+ch4GWP = 28  # GWP of Methane
+mmbf2mcf = 5.44  # From McIver and Morgan
+
+#Bioenergy percentages from McIver
+bio_pct = pd.read_sql('select "index" as year,"Bioenergy"/100 as biopct from mciver_bio where "Bioenergy" is not null',
+                      sqlitedb()['cx'])
+bio_dict = bio_pct.set_index('year').to_dict('index')
+
+# Sathre DF with logging residue utilization in bioenergy
+
+HWu = pd.read_sql('''SELECT *
+                        FROM so4
+                        WHERE harvestslash = "X"
+                        AND processingresidues = "X" 
+                        AND "post-usewoodproduct" = "X"
+                        AND stumps is null''',
+                  sqlitedb()['cx'],
+                  index_col='index')
+
+# Sathre DF without logging residue utilization in bioenergy
+HWo = pd.read_sql('''SELECT *
+                        FROM so4
+                        WHERE harvestslash is null
+                        AND processingresidues = "X"
+                        AND stumps is null
+                        AND "post-usewoodproduct" = "X"''',
+                  sqlitedb()['cx'],
+                  index_col='index')
+
+wood_dens = gData('138FWlGeW57MKdcz2UkWxtWV4o50SZO8sduB1R6JOFp8', 1297253755)
+
+constants = {'me': {'value': 0.5,
+                    'desc': 'Mill Efficiency'},
+             'DFu': {'value': np.average(HWu.df),
+                     'desc': 'Displacement factor with logging residual utilization',
+                     'source': '''\cite{Sathre2010}'''},
+             'DFo': {'value': np.average(HWo.df),
+                     'desc': 'Displacement factor without logging residual utilization'},
+             'wDens': {'value': sum(wood_dens.pct/100 * wood_dens.density_lbscuft),
+                       'units': 'lbs/cuft',
+                       'desc': 'average harvested wood density weighted by species harvested',
+                       'source': '\cite{Mciver2012}'}
+             }
 
 
 def csv2sqlite(dbcon, csvfile, ex='replace'):
@@ -156,32 +200,34 @@ def pm2bcPiles(pm, est='estimate', gwp=3200):
     tcSlow = tcSest - (sTCcv * tcSest)
     bcSlow = tcSlow * sBC - ((tcSlow * sBC)*sBCcv)
 
-
     results = {'estimate': (bcFest + bcSest) * gwp,
-               'high': (bcFhigh + bcShigh) *gwp,
-               'low': (bcFlow +bcSlow) * gwp
+               'high': (bcFhigh + bcShigh) * gwp,
+               'low': (bcFlow + bcSlow) * gwp
                }
 
     return results[est]
 
 
-def me(yr,vol):
+def me(yr, vol):
     """
-    calculates mill residues for a give volume of roundwood harvested (mmbf) amd year
+    calculates mill residues for a give volume of
+    roundwood harvested (mmbf) amd year
     """
-    me70 = 0.42 #Mill efficiency in 1970
-    me11 = 0.48 #Mill efficiency in 2011
+    me70 = 0.42  # Mill efficiency in 1970
+    me11 = 0.48  # Mill efficiency in 2011
     ann = (me11 - me70)/(2011-1970)
     if yr <= 1970:
         return vol * (me70-((1970-yr)*ann))
     if yr >= 1970:
         return vol * (me70+((yr-1970)*ann))
 
-def lr(yr,vol):
+
+def lr(yr, vol):
     """
-    probabilistic estimate of the volume of logging residuals for a given year and roundwood harvest in MMBF
+    probabilistic estimate of the volume of
+    logging residuals for a given year and roundwood harvest in MMBF
     """
-    lr04 = np.random.normal(0.0302,0.0123)
+    lr04 = np.random.normal(0.0615, 0.0229)
     # 72% reduction in logging residue production between 1990 and 2011)
     ann = 0.72/(2011-1990)
     lr90 = lr04 + (lr04*0.72)
@@ -189,7 +235,20 @@ def lr(yr,vol):
     # if year is before 1990, assume lr from 1990
     if yr < 1990:
         return vol * lr90
-    if yr >= 1990 and yr <=2004 :
+    if yr >= 1990 and yr <= 2004:
         return vol * (lr04 + (lr04*((2004-yr)*ann)))
     if yr > 2004:
         return vol * (lr04 - (lr04*((yr-2004)*ann)))
+
+
+def bioPct(year):
+    """
+    returns an estimate of bioenergy consumption for a given year
+    """
+    bd = bio_dict
+    if str(year) in bd.keys():
+        return bd[str(year)]['biopct']
+    elif year < 1980:
+        return 0
+    else:
+        return np.mean([bd[i]['biopct'] for i in bd.keys()])
